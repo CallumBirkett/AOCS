e36e4328b68a7a7a00c01a16777cd6c332b91519
CallumBirkett <63651513+CallumBirkett@users.noreply.github.com>
2026-01-14 11:39:17 +0000

    Stopped tracking build artifacts'

--- build/CMakeFiles/aocs.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for aocs.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/aocs.dir/src/aocs_status.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/aocs.dir/src/aocs_status.c.o: \
-  /Users/callumbirkett/Documents/aocs/src/aocs_status.c \
-  /Users/callumbirkett/Documents/aocs/include/aocs_status.h
